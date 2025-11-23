7d89063bfe4f6fef07a102a40a19ac6dd852e385
mrDonald009 <mrdonald009@gmail.com>
2025-11-23 14:49:19 +0300

    added # Создайте файл .gitattributes с содержимым:

---
+++ .gitattributes.py
@@ -0,0 +1,7 @@
+# Создайте файл .gitattributes с содержимым:
+*.py text eol=lf
+*.md text eol=lf
+*.txt text eol=lf
+*.js text eol=lf
+*.html text eol=lf
+*.css text eol=lf
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#import
 import telebot
 import config
 import os
